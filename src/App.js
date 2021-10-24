938ecf9af8978d3cc0af9c1a473b05a58274e760
Ian Shepherd <50464036+shepherx@users.noreply.github.com>
2021-10-24 11:05:31 -0600

    ported to react

--- src/App.js
+++ src/App.js
@@ -1,23 +1,106 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div class="container-main">
+      <section id="showcase">
+        <div class="video-container">
+          <video
+            src="/coverr-arctic-waves-157519728777211.mp4"
+            autoPlay
+            muted
+            playsinLine
+            loop
+            type="video/mp4"
+          ></video>
+        </div>
+        <div class="content">
+          <h1>technical support & web development</h1>
+          <div id="subtext">
+            <h3>scottsdale, arizona</h3>
+          </div>
+        </div>
+      </section>
+      <section id="about-section">
+       
+          
+        
+        <div class="about-text">
+          <div class="p-item">
+              
+           
+            <div class="bio-wrapper">
+              <h1 class="inside">
+                bio &#128507; &#127797; |
+              </h1>
+            
+            </div>
+            <p class="inside-p">
+              Hey everyone, my name is Ian Shepherd, and I work at Medallia. I
+              live in Scottsdale, Arizona.
+              <br />
+              <br />
+              I graduated from Colorado State University with a Bachelor's in
+              Computer Science. My skills include HTML, CSS, JavaScript, React, Vue,
+              and Python.
+              <br />
+            </p>
+          </div>
+          <div class="i-item">
+            <img src="/pic.jpg" alt="" />
+          </div>
+        </div>
+      </section>
+
+      <section class="second-bg">
+        <div class="second-img-container">
+          <img src="./root/img/tim-oun-jY_W5x74GTs-unsplash.png" alt="" />
+        </div>
+      </section>
+
+      <section class="technologies">
+       
+        <div class="tech-container">
+          <i class="fab fa-html5"></i>
+          <i class="fab fa-css3-alt"></i>
+          <i class="fab fa-js-square"></i>
+          <i class="fab fa-python"></i>
+        </div>
+      </section>
+
+      <section id="contact-me">
+        <div class="contact-container">
+          <div class="contact-text">
+            <h1>let's get in contact</h1>
+            <br />
+            <p>ian shepherd</p>
+            <p>scottsdale, az</p>
+          
+            <p>ianshepherx@gmail.com</p>
+            <a target="_blank" rel="noreferrer" href="https://github.com/shepherx"
+              ><i class="fab fa-github"></i
+            ></a>
+
+            <a target="_blank" rel="noreferrer" href="https://www.linkedin.com/in/ian-shepherd/"
+              ><i class="fab fa-linkedin-in"></i
+            ></a>
+            <a target="_blank" rel="noreferrer" href="https://www.instagram.com/_shepherx/"
+              ><i class="fab fa-instagram"></i
+            ></a>
+          </div>
+          <div class="form-div">
+            <form data-netlify="true" method="post" name="contact">
+              Name:<br />
+              <input type="text" name="name" /><br />
+              Email:<br />
+              <input type="text" name="mail" /><br />
+              Comment:<br />
+              <input type="text" name="comment" size="50" /><br /><br />
+              <input id="btn-email" type="submit" value="Send" />
+            </form>
+          </div>
+        </div>
+      </section>
     </div>
   );
 }
